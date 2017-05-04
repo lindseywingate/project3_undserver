f87000ce279299522ebe15876830fbd69c191273
lindsey wingate <lindseymwingate@gmail.com>
2017-05-04 15:52:24 -0500

    got it working!! it recognizes the forms on the app to donate :D

--- applicationtodonate.php
+++ applicationtodonate.php
@@ -7,6 +7,13 @@ if(isset($_POST['rooms'])) {
 	for($i=0; $i<count($rooms); $i++) {
 		echo $rooms[$i]."<br />";
 	}
+	echo $_POST['bedrooms'];
+	echo $_POST['nummasterbedrooms'];
+	echo $_POST['nummasterbathrooms'];
+	echo $_POST['numaptbedrooms'];
+	echo $_POST['numaptbathrooms'];
+	echo $_POST['numhousebedrooms'];
+	echo $_POST['numhousebathrooms'];
 }
 
 
