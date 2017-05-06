bbd03877afb4da9423f17decf5964cf2bd48ff25
Lindsey Wingate <lindseymwingate@gmail.com>
2017-05-06 16:20:33 -0600

    more stuff

---
+++ js/bookspace.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#home').on('click', function() {
+		window.location.href=("http://people.aero.und.edu/~lwingate/457/3/home.html");
+	});
+	$('#logout').on('click', function() {
+		window.location.href=("http://people.aero.und.edu/~lwingate/457/3/index.html");
+	});
+});
+	
