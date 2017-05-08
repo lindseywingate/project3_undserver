239f84976dc1434cfccce05cd95bf95887fe56a5
lindsey wingate <lindseymwingate@gmail.com>
2017-05-08 13:07:25 -0500

    testing it offline

--- getsearch.php
+++ getsearch.php
@@ -1,23 +1,26 @@
 <?php
-//$data = $_POST["action"];
-$data = 'testing2';
+if(isset($_POST["place"])) {
+	$data = $_POST["place"];
+	$username = "lindseywingate";
+	$password = "password";
+	$host = "mysqldev.aero.und.edu";
+	$conn = mysql_connect($host, $username, $password);
 
-$username = "lindseywingate";
-$password = "password";
-$host = "mysqldev.aero.und.edu";
-$conn = mysql_connect($host, $username, $password);
+	if($conn) {
+		mysql_select_db("lindseywingate", $conn);
+		$query = "SELECT latitude, longitude from places WHERE name = '$data'";
 
-if($conn) {
-	mysql_select_db("lindseywingate, $conn");
-	$query = "SELECT latitude, longitude from places WHERE name = '$data'";
-	$result = mysql_query($query, $conn);
-	$num = mysql_numrows($result);
-		print $num;
-//	while($row=mysql_fetch_assoc($result)) {
-//		print $row['latitude'];
-//		print $row['longitude'];
-//	}
-
+		$result = mysql_query($query, $conn) or die(mysql_error());
+		$num = mysql_numrows($result);
+			print $num;
+		while($row=mysql_fetch_assoc($result)) {
+			print $row['latitude'];
+			print $row['longitude'];
+		}
+		return $row;
+	}
 }
-
+else {
+	return "This didn't work.";
+}
 ?>
